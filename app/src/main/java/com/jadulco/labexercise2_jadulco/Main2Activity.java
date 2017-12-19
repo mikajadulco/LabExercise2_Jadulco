738aeda3f61c82a0c67fe75372835d57e015a92c
mikajadulco <2014070086@ust-ics.mygbiz.com>
2017-12-19 09:11:02 +0800

    updated

--- app/src/main/java/com/jadulco/labexercise2_jadulco/Main2Activity.java
+++ app/src/main/java/com/jadulco/labexercise2_jadulco/Main2Activity.java
@@ -1,13 +1,75 @@
 package com.jadulco.labexercise2_jadulco;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Button btLoadShared, btLoadInternal, btClear, btBack;
+    TextView tvDisplay;
+    SharedPreferences preferences;
+    FileInputStream fis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btLoadShared = (Button) findViewById(R.id.btn_loadShared);
+        btLoadInternal = (Button) findViewById(R.id.btn_loadInternal);
+        btClear = (Button) findViewById(R.id.btn_clear);
+        btBack = (Button) findViewById(R.id.btn_back);
+        tvDisplay = (TextView) findViewById(R.id.tv_display);
+        preferences = getSharedPreferences("sharedText", MODE_PRIVATE);
     }
+
+    public void loadShared(View view){
+        String user = preferences.getString("user","");
+        String password = preferences.getString("pwd","");
+        tvDisplay.setText("User is " + user + " and the password is " + password);
+    }
+
+    public void loadInternal (View view){
+        StringBuffer buffer = new StringBuffer();
+        int read = 0;
+        try {
+            fis = openFileInput("output.txt");
+            while ((read = fis.read()) != -1){
+                buffer.append((char)read);
+            }
+            fis.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        tvDisplay.setText(buffer.toString());
+    }
+
+    public void clear(View view){
+
+        tvDisplay.setText("");
+    }
+
+    public void back (View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/jadulco/labexercise2_jadulco/MainActivity.java
+++ app/src/main/java/com/jadulco/labexercise2_jadulco/MainActivity.java
@@ -1,13 +1,76 @@
 package com.jadulco.labexercise2_jadulco;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etUser, etPass;
+    Button btnShared, btnInternal, btnNext;
+    FileOutputStream fos;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUser = (EditText) findViewById(R.id.et_username);
+        etPass = (EditText) findViewById(R.id.et_password);
+        btnShared = (Button) findViewById(R.id.btn_sharedPref);
+        btnInternal = (Button) findViewById(R.id.btn_internalStorage);
+        btnNext = (Button) findViewById(R.id.btn_next);
+
+        preferences = getSharedPreferences("sharedText", MODE_PRIVATE);
     }
+
+    public void saveShared (View view) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("user",etUser.getText().toString());
+        editor.putString("pwd",etPass.getText().toString());
+        editor.commit();
+        Toast.makeText(this, "Saved in Shared Preferences!", Toast.LENGTH_LONG).show();
+    }
+
+    public void saveInternal (View view){
+        String user = "Username is  " + etUser.getText().toString() + " | ";
+        String pass = "Password is  " + etPass.getText().toString() + " ";
+
+        try {
+            fos = openFileOutput("output.txt", Context.MODE_PRIVATE);
+            fos.write(user.getBytes());
+            fos.write(pass.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Toast.makeText(this, "Saved in Internal Storage!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void nextAct (View view){
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
+
 }
